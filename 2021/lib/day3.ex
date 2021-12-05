acff0eced2ff9dbfb5fed49ba1f1af92c03fae9a
Fabrice Claeys <fabrice.claeys@symbol-it.fr>
2021-12-05 15:59:51 +0100

    refacto day3 part1

--- 2021/lib/day3.ex
+++ 2021/lib/day3.ex
@@ -1,4 +1,24 @@
 defmodule Day3 do
+  defp max_occurence_of_0_or_1(list) do
+    count_zero = Enum.count(list, fn d -> d == "0" end)
+    count_one = Enum.count(list, fn d -> d == "1" end)
+
+    case count_zero > count_one do
+      true ->
+        "0"
+
+      false ->
+        "1"
+    end
+  end
+
+  defp inverted_0_or_1(value) do
+    case value do
+      "0" -> "1"
+      "1" -> "0"
+    end
+  end
+
   def binary_diagnostic_1 do
     # input = """
     # 00100
@@ -16,51 +36,23 @@ defmodule Day3 do
     # """
 
     # String.split(input, "\n", trim: true)
-    gamma =
+
+    data =
       File.stream!(Path.join("data", "day3.txt"))
       |> Stream.flat_map(&String.split(&1, "\n", trim: true))
       |> Stream.map(&String.graphemes/1)
-      # |> Stream.map(fn b -> Integer.parse(b, 2) end)
-      # |> Stream.map(fn {value, ""} -> value end)
       |> Stream.zip()
       |> Stream.map(&Tuple.to_list/1)
-      |> Stream.map(fn l ->
-        count_zero = Enum.count(l, fn d -> d == "0" end)
-        count_one = Enum.count(l, fn d -> d == "1" end)
 
-        case count_zero > count_one do
-          true ->
-            "0"
-
-          false ->
-            "1"
-        end
-      end)
-      # |> Enum.group_by(fn k -> k end)
+    gamma =
+      data
+      |> Stream.map(&max_occurence_of_0_or_1/1)
       |> Enum.reduce("", fn r, acc -> acc <> r end)
       |> String.to_integer(2)
 
     epsilon =
-      File.stream!(Path.join("data", "day3.txt"))
-      |> Stream.flat_map(&String.split(&1, "\n", trim: true))
-      |> Stream.map(&String.graphemes/1)
-      # |> Stream.map(fn b -> Integer.parse(b, 2) end)
-      # |> Stream.map(fn {value, ""} -> value end)
-      |> Stream.zip()
-      |> Stream.map(&Tuple.to_list/1)
-      |> Stream.map(fn l ->
-        count_zero = Enum.count(l, fn d -> d == "0" end)
-        count_one = Enum.count(l, fn d -> d == "1" end)
-
-        case count_zero > count_one do
-          true ->
-            "1"
-
-          false ->
-            "0"
-        end
-      end)
-      # |> Enum.group_by(fn k -> k end)
+      data
+      |> Stream.map(&inverted_0_or_1(max_occurence_of_0_or_1(&1)))
       |> Enum.reduce("", fn r, acc -> acc <> r end)
       |> String.to_integer(2)
 
